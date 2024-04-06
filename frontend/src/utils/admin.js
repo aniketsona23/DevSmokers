0b45bf3e752f13a17b9a4cec45c91bc656d4c530
Divyam Gupta <divyam.dg2003@gmail.com>
2024-04-06 23:01:36 +0530

    changes utils

---
+++ frontend/src/utils/admin.js
@@ -0,0 +1,28 @@
+viewAdminDashApplications=[
+   
+    {field:"studentName",header:"Student Name",width:20},
+    {field:"projectTitle",header:"Project Title",width:20},
+    {field:"studentId",header:"Student ID",width:20},
+    {field:"gpsrn",header:"GPSRN",width:20},
+    {field:"date",header:"Date",width:20},
+    {field:"status",header:"Status",width:20},
+    {field:"",header:"",width:20},
+
+
+   
+
+]
+
+viewAdminDashProjects=[
+
+    {field:"projectName",header:"Project Name",width:20},
+    {field:"projectId",header:"Project ID",width:20},
+    {field:"no.ofStudents",header:"No. of Students",width:20},
+    {field:"gpsrn",header:"GPSRN",width:20},
+    {field:"date",header:"Date",width:20},
+    {field:"status",header:"Status",width:20},
+    {field:"",header:"",width:20},
+
+
+
+]
